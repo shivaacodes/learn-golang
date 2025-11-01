ffa337351f009ffb7a23d41bd96c0f0cee464983
Shiva Sajay <shivasajay007@gmail.com>
2025-11-01 12:29:45 +0530

    channels

--- chapter-1-intro/channel.go
+++ chapter-1-intro/channel.go
@@ -7,12 +7,12 @@ import "fmt"
 // channels: is like a tunnel which transfers data between goroutines.
 // it is a container datatype. we have to specify the type of the data.
 
-func sum(s []int, c chan int) { // here c is a channel which take int datatype
+func sum(s []int, c chan int) { // here, c is a channel which take int datatype
 	sum := 0
 	for _, v := range s {
 		sum += v
 	}
-	c <- sum // sending teh result to teh channel (here; sending sum to c)
+	c <- sum // sending the result to the channel (here; sending sum to c)
 }
 
 // 5 ref types : slices, maps, funcs, channels, pointers
@@ -23,7 +23,7 @@ func main() {
 
 	// spawn 2 goroutine
 	go sum(s[:len(s)/2], c) // sum of first half
-	go sum(s[len(s)/2:], c) // sum of first half
+	go sum(s[len(s)/2:], c) // sum of second half
 
 	x, y := <-c, <-c // receive data from c (blocking execution)
 
@@ -34,7 +34,7 @@ func main() {
 // 2 types of channels (high-level difference)
 
 // buffered : queue; goroutines can push values into a queue, sequentially until the size is filled.
-// (keep pushing it wont block)
+// (keep pushing; it wont block)
 
 // unbuffered : no queue; just a tunnel. until the producer and the consumer (goroutines) are in sync.
 // until the recieving goroutine take the value; it is blocked. usecase: for forced synchronization
