07528aafc85f7e898197049d73ea5273dc0b0ea5
Shiva Sajay <shivasajay007@gmail.com>
2026-03-11 13:12:45 +0530

    before implementing fan_out_fan_in

---
+++ fan_out_fan_in/image_processing/image_processing.go
@@ -0,0 +1,46 @@
+package imageprocessing
+
+import (
+	"fmt"
+	"image"
+	"image/jpeg"
+	"os"
+
+	"github.com/nfnt/resize"
+)
+
+func ReadImage(path string) image.Image {
+	inputFile, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer inputFile.Close()
+
+	img, _, err := image.Decode(inputFile)
+	if err != nil {
+		fmt.Println(path)
+		panic(err)
+	}
+
+	return img
+}
+
+func WriteImage(path string, img image.Image) {
+	outputFile, err := os.Create(path)
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	err = jpeg.Encode(outputFile, img, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func Resize(img image.Image) image.Image {
+	newWidth := uint(500)
+	newHeight := uint(500)
+	resizedImg := resize.Resize(newWidth, newHeight, img, resize.Lanczos3)
+	return resizedImg
+}
